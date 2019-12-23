a6fc322ef10ac45c8d8caf567ad2bcc18816651c
Sarah Vessels <cheshire137@gmail.com>
2019-12-23 14:24:15 -0600

    Use one line for displaying commands

--- cmd/stardewcal/main.go
+++ cmd/stardewcal/main.go
@@ -36,35 +36,35 @@ func main() {
 	lines := cal.EventsSummary(events)
 	fmt.Println(cal.DaySheet(lines...))
 
-	exitChoice := 3
-	userChoice := 0
-	for userChoice != exitChoice {
-		fmt.Println("1) Go to next day")
-		fmt.Println("2) Go to previous day")
-		fmt.Println("3) Exit")
+	userChoice := "a"
+	for userChoice != "x" && userChoice != "X" {
+		fmt.Println("(n)ext day     (p)revious day     e(x)it")
 		fmt.Print("> ")
-		_, err := fmt.Scanf("%d", &userChoice)
+		_, err := fmt.Scanf("%s", &userChoice)
 		if err != nil {
-			fmt.Println("Error: invalid choice, please choose 1-3:")
+			fmt.Println("Error: invalid choice, please choose n, p, or x:")
 		}
 
-		if userChoice == 1 {
+		isNextDayChoice := userChoice == "n" || userChoice == "N"
+		isPrevDayChoice := userChoice == "p" || userChoice == "P"
+
+		if isNextDayChoice {
 			err := cal.NextDay()
 			if err != nil {
 				fmt.Println(err)
 				os.Exit(1)
 			}
-		} else if userChoice == 2 {
+		} else if isPrevDayChoice {
 			err := cal.PreviousDay()
 			if err != nil {
 				fmt.Println(err)
 				os.Exit(1)
 			}
-		} else if userChoice != exitChoice {
+		} else if userChoice != "x" && userChoice != "X" {
 			fmt.Println("Error: invalid choice, please choose 1-3:")
 		}
 
-		if userChoice == 1 || userChoice == 2 {
+		if isNextDayChoice || isPrevDayChoice {
 			events, err := cal.CurrentEvents()
 			if err != nil {
 				fmt.Println(err)
